cadb36deb153a797192c05dd25e28774e907ac69
jingpengwu <jingpeng.wu@gmail.com>
2017-10-23 09:07:09 -0400

    error handling of untracable cell id

--- scripts/skeletonize.jl
+++ scripts/skeletonize.jl
@@ -72,7 +72,15 @@ function main()
         while true 
             handle, body = fetch(sqsChannel) 
             d = JSON.parse( body )
-            trace(d["id"]; swcDir=d["swcdir"], mip=d["mip"])
+            try 
+                trace(d["id"]; swcDir=d["swcdir"], mip=d["mip"])
+            catch err 
+                if isa(err, KeyError)
+                    warn("not tracable cell id: $(d["id"])") 
+                else 
+                    rethrow()
+                end 
+            end 
             delete!(sqsChannel, handle)
         end 
     else 
