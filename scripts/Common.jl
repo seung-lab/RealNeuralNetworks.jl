02d6bea56e52d080a9563a4b0f3a7667317e62ac
jingpengwu <jingpeng.wu@gmail.com>
2017-10-22 21:23:01 -0400

    fix some typo in Common

--- scripts/Common.jl
+++ scripts/Common.jl
@@ -2,9 +2,9 @@ module Common
 
 using ArgParse 
 export parse_commandline 
-
+export SEGMENT_ID, MIP, VOXEL_SIZE, SEGMENTATION_LAYER 
 # this is the mip level 4
-const SEGMENTAT_ID = 92540687
+const SEGMENT_ID = 92540687
 const MIP = UInt32(4)
 const VOXEL_SIZE = (5,5,45)
 const SEGMENTATION_LAYER ="gs://neuroglancer/zfish_v1/consensus-20170928"
@@ -25,7 +25,7 @@ function parse_commandline()
         "--neuronid", "-i"
             help = "the segment id to skeletonize"
             arg_type = Int
-            default = SEGMENTAT_ID #77497
+            default = SEGMENT_ID #77497
         "--swcdir", "-s"
             help = "the directory to store swc file"
             arg_type = String
