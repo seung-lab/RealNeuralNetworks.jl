7064f69759ed9371e19c402fc82af125c3ae010f
jingpengwu <jingpeng.wu@gmail.com>
2018-01-24 12:08:37 -0500

    show manifest test time

--- test/Manifests.jl
+++ test/Manifests.jl
@@ -20,7 +20,8 @@ end
 @testset "test manifest iteration" begin
     manifest = Manifest("gs://neuroglancer/zfish_v1/consensus-20170829/mesh_mip_4", 
                         "$(cellId):0", "gs://neuroglancer/zfish_v1/consensus-20170829/80_80_45")
-    nodeNet = Manifests.trace(manifest, cellId)
+    println("trace a cell...")
+    @time nodeNet = Manifests.trace(manifest, cellId)
     swc = SWC( nodeNet )
     SWCs.stretch_coordinates!(swc, VOXEL_SIZE)
     SWCs.save(swc, "/tmp/$(cellId).swc")
