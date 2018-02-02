8ee3c96c85970bf3c9a9c914ed6536016c0d8061
jingpengwu <jingpeng.wu@gmail.com>
2018-02-01 22:57:54 -0500

    use AWSSDK rather than AWSSQS which is not working now. use batch mode to speed up

--- scripts/taskproducer.jl
+++ scripts/taskproducer.jl
@@ -1,8 +1,12 @@
 #!/usr/bin/env julia
 include("Common.jl")
 using .Common 
-using SQSChannels 
 using JSON
+using AWSSDK.SQS 
+using AWSCore
+using ProgressMeter
+
+const AWS_CREDENTIAL = AWSCore.aws_config()
 
 function main()
     args = parse_commandline()
@@ -10,15 +14,29 @@ function main()
     @assert args["idlistfile"] != nothing
      
     idList = map( parse, split( readstring(args["idlistfile"]), "\n" ) )
-    argList = Vector{String}()
+    messageList = Vector{String}()
     for id in idList 
         args["id"] = id 
-        push!(argList, JSON.json(args))
+        push!(messageList, JSON.json(args))
     end
 
     # put to AWS SQS queue
-	sqsChannel = SQSChannel( args["sqsqueue"] )
-    put!(sqsChannel, argList )
+    const queueUrl = SQS.get_queue_url( QueueName=args["sqsqueue"] )["QueueUrl"]
+
+#    for message in messageList
+#        @show message
+#        SQS.send_message( QueueUrl=queueUrl, MessageBody=message)
+#    end
+
+    @showprogress 1 "producing tasks..." for i in 1:10:length(messageList)
+        messages = messageList[i:min(i+9, length(messageList))]
+        messageBatch = map((x,y)->["Id"=>string(i+x-1), "MessageBody"=>y],
+                                                1:length(messages), messages)
+        SQS.send_message_batch(AWS_CREDENTIAL; QueueUrl=queueUrl, 
+                               SendMessageBatchRequestEntry=messageBatch)
+    end 
+
+    nothing
 end
 
 main()
