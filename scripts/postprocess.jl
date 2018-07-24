ef7c8a3dfb65c25475f0a3d713eae708dd946f73
jingpengw <jingpeng.wu@gmail.com>
2018-07-24 11:07:03 -0400

    make postprocess.jl work

--- scripts/postprocess.jl
+++ scripts/postprocess.jl
@@ -1,24 +1,63 @@
 #!/usr/bin/env julia
+using ProgressMeter
+using ArgParse
 
 using RealNeuralNetworks
 using RealNeuralNetworks.SWCs
 using RealNeuralNetworks.Neurons 
 
-include("Common.jl"); using Common
-
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin 
+        "--inputdir", "-i"
+            help = "the directory to store swc file"
+            arg_type = String
+            default = "/tmp"
+        "--outputpath", "-o"
+            help = "the path to store outputs"
+            arg_type = String
+            default = "/tmp"
+        "--resample-node-distance", "-d"
+            help = "mip level of the dataset" 
+            arg_type = Float32
+            default = Float32(100)
+    end
+    return parse_args(s)
+end 
+ 
 function main()
     args = parse_commandline()
 
-    for filename in dirname(args["swcdir"])
-        @show filename
-        neuron = Neurons.load_swc_bin(joinpath(args["swcdir"], filename))
+    @showprogress 1 "postprocessing..." for fileName in readdir(args["inputdir"])
+        fullName = joinpath(expanduser(args["inputdir"]), fileName)
+        if endswith(fileName, "swc.bin")
+            neuron = Neurons.load_swc_bin(fullName)
+        elseif endswith(fileName, ".swc")
+            neuron = Neurons.load_swc(fullName)
+        elseif isdir(fullName)
+            warn("this is a directory: $(fileName)")
+        else 
+            warn("unsupported file format: $(fileName)")
+            continue 
+        end 
         neuron = Neurons.remove_hair(neuron)
         neuron = Neurons.remove_redundent_nodes(neuron)
         neuron = Neurons.remove_subtree_in_soma(neuron)
         neuron = Neurons.remove_terminal_blobs(neuron)
-        neuron = Neurons.resample(neuron, Float32(100))
-        Neurons.save_swc_bin(neuron, joinpath(args["outputpath"], filename))
-        Neurons.save_swc(neuron, joinpath(args["outputpath"]*"_swc", filename))
+        neuron = Neurons.resample(neuron, args["resample-node-distance"])
+
+        # output file 
+        outputName = joinpath(expanduser(args["outputpath"]*"_swc_bin"), fileName)
+        if !isdir(dirname(outputName))
+            mkdir(dirname(outputName))
+        end 
+        Neurons.save_swc_bin(neuron, outputName)
+        
+        outputName = joinpath(expanduser(args["outputpath"]*"_swc"), fileName)
+        if !isdir(dirname(outputName))
+            mkdir(dirname(outputName))
+        end 
+        Neurons.save_swc(neuron, outputName)
     end
 end
 
