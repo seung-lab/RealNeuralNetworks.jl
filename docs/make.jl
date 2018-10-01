f8c367438f0559b1061ab8e839adcea113d5cc44
jingpengw <jingpeng.wu@gmail.com>
2018-10-01 11:41:44 -0400

    update make.jl to add pages, otherwise there will be nothing there

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,27 @@ makedocs(
 	modules=[RealNeuralNetworks, Neurons, Segments, Synapses, SWCs],
 	sitename="RealNeuralNetworks.jl",
 	authors="Jingpeng Wu",
-	format=:html
+	format=:html,
+    pages = [
+        "Home" => "index.md",
+        "Manual" => Any[
+            "Getting Started" => "man/getting_started.md",
+            "man/examples.md",
+            "man/references.md",
+        ],
+        "Library" => Any[
+            "Public" => "lib/public.md",
+            hide("Internals" => "lib/internals.md", Any[
+                "lib/internals/Manifests.md",
+                "lib/internals/NodeNets.md",
+                "lib/internals/PointArrays.md",
+            ])
+        ],
+        "man/contributing.md",
+    ],
+    # use clean URLs, unless built as a "local" build 
+    html_prettyurls = !("local" in ARGS),
+    html_canonical = "https://seung-lab.github.io/RealNeuralNetworks.jl/latest",
 )
 
 deploydocs(
